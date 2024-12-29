800f093a1b869ca2a3c898bd0466519f249d9137
Sumit021995 <kaku.saurav@gmail.com>
2024-12-29 15:04:36 +0530

    ff browser

--- com.ecom.Shopify1/src/test/java/section2/FileUpload4.java
+++ com.ecom.Shopify1/src/test/java/section2/FileUpload4.java
@@ -9,18 +9,25 @@ import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class FileUpload4 {
 	public static void main(String[] args) throws AWTException, InterruptedException {
-		WebDriver driver=new ChromeDriver();
+//		WebDriver driver=new ChromeDriver();
+		System.setProperty("webdriver.gecko.driver","C:\\Users\\sumit\\Downloads\\geckodriver-v0.35.0-win64\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.manage().window().maximize();
 		driver.get("https://demoapps.qspiders.com/ui?scenario=1");
 		driver.findElement(By.xpath("//section[text()='Popups']")).click();
+		Thread.sleep(2000);
+		
 		driver.findElement(By.xpath("//section[text()='File Uploads']")).click();
+		Thread.sleep(2000);
 		driver.findElement(By.xpath("//a[text()='Custom Button']")).click();
+		Thread.sleep(2000);
 		StringSelection path = new StringSelection("C:\\Users\\sumit\\OneDrive\\Desktop\\screenshot\\image.png");
+		Thread.sleep(2000);
 		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(path, null);
 		
 		driver.findElement(By.xpath("//div[text()='Upload File']")).click();
