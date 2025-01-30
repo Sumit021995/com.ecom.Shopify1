f3f6cffad2beb1b5a6857406931e5c3852d65ec4
Sumit021995 <kaku.saurav@gmail.com>
2025-01-30 08:52:40 +0530

    new commit

--- com.ecom.Shopify1/src/test/java/relativeLocator/RightRelativelocator.java
+++ com.ecom.Shopify1/src/test/java/relativeLocator/RightRelativelocator.java
@@ -1,7 +1,6 @@
 package relativeLocator;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.locators.RelativeLocator;
