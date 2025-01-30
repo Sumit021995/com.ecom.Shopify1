52ca9e934b97f7fc8503b7036f0b6831c0e7e120
Sumit021995 <kaku.saurav@gmail.com>
2025-01-30 08:55:36 +0530

    new commit

--- com.ecom.Shopify1/src/test/java/relativeLocator/RightRelativelocator.java
+++ com.ecom.Shopify1/src/test/java/relativeLocator/RightRelativelocator.java
@@ -14,7 +14,8 @@ public class RightRelativelocator {
 //		driver.findElement(By.name("search_query")).sendKeys("Kaal Bhairav Theme Music",Keys.ENTER);
 //		Thread.sleep(3000);
 		driver.findElement(By.id("small-searchterms")).sendKeys("computer");
-		driver.findElement(RelativeLocator.with(By.tagName("//input")).toRightOf(By.id("small-searchterms"))).click();
+		Thread.sleep(3000);
+		driver.findElement(RelativeLocator.with(By.tagName("input")).toRightOf(By.id("small-searchterms"))).click();
 	}
 }
 
