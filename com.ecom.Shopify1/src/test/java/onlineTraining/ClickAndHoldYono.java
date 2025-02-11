1da30346ee74fb6689da3b95cd3558d4108382b4
Sumit021995 <kaku.saurav@gmail.com>
2025-02-11 08:18:06 +0530

    new commit

--- com.ecom.Shopify1/src/test/java/onlineTraining/ClickAndHoldYono.java
+++ com.ecom.Shopify1/src/test/java/onlineTraining/ClickAndHoldYono.java
@@ -7,16 +7,25 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class ClickAndHoldYono {
-	public static void main(String[] args)
+	public static void main(String[] args) throws InterruptedException
 	{
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("https://yonobusiness.sbi/login/yonobusinesslogin");
 		driver.findElement(By.id("userName")).sendKeys("asdfgh",Keys.TAB,"abc@123");
-		WebElement EyeIcon = driver.findElement(By.xpath("//div[@class='showPassword shownhide']"));
+		WebElement eyeIcon = driver.findElement(By.xpath("//div[@class='showPassword shownhide']"));
+		Thread.sleep(2000);
+		Actions act = new Actions(driver);
+		act.moveToElement(eyeIcon).clickAndHold().perform();
+//		act.clickAndHold(eyeIcon).perform();
+		Thread.sleep(2000);
+		act.moveToElement(eyeIcon).release().perform();
+//		act.release(eyeIcon).perform();
+		
 		
 		
 	}
