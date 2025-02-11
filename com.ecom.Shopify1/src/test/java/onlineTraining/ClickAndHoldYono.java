6c02a6ed9eebb173a9d549044bea483d5510e2f4
Sumit021995 <kaku.saurav@gmail.com>
2025-02-11 07:58:45 +0530

    new commit

--- com.ecom.Shopify1/src/test/java/onlineTraining/ClickAndHoldYono.java
+++ com.ecom.Shopify1/src/test/java/onlineTraining/ClickAndHoldYono.java
@@ -1,8 +1,17 @@
 package onlineTraining;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class ClickAndHoldYono {
 	public static void main(String[] args)
 	{
 		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://yonobusiness.sbi/login/yonobusinesslogin");
+		
 	}
 }
