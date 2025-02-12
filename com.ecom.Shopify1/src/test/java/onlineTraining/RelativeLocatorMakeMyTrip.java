140f72a67d3d3366430fcd8b7791932efed10ee6
Sumit021995 <kaku.saurav@gmail.com>
2025-02-12 11:10:42 +0530

    new commit

--- com.ecom.Shopify1/src/test/java/onlineTraining/RelativeLocatorMakeMyTrip.java
+++ com.ecom.Shopify1/src/test/java/onlineTraining/RelativeLocatorMakeMyTrip.java
@@ -9,23 +9,41 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class RelativeLocatorMakeMyTrip {
-	@FindBy(xpath=("")) private WebElement mmtPopUp;
+	@FindBy(xpath=("//span[@class='commonModal__close']")) private WebElement mmtPopUp;
 	public WebElement getMmtPopUp()
 	{
 		return mmtPopUp;
+	}
+	@FindBy(xpath=("//li[text()='Round Trip']")) private WebElement roundTripText;
+	public WebElement getRoundTripText()
+	{
+		return roundTripText;
 	}
 	public RelativeLocatorMakeMyTrip(WebDriver driver) {
 		PageFactory.initElements(driver,this);
 	}
 	public void removePopup()
 	{
-		
+		getMmtPopUp().click();
 	}
-	public static void main(String[] args) {
+	public void clickOnRoundTripText()
+	{
+		getRoundTripText().click();
+	}
+	public static void main(String[] args) throws InterruptedException {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("https://www.makemytrip.com/");
+		RelativeLocatorMakeMyTrip r = new RelativeLocatorMakeMyTrip(driver);
+		r.removePopup();
+		r.clickOnRoundTripText();
+		Thread.sleep(2000);
+		driver.quit();
+		
+		
+		
+		
 		
 	}
 }
