6321a49198e0e04370ee5838eb5782741075cdd2
Sumit021995 <kaku.saurav@gmail.com>
2025-02-12 10:46:29 +0530

    new commit

--- com.ecom.Shopify1/src/test/java/onlineTraining/RelativeLocatorMakeMyTrip.java
+++ com.ecom.Shopify1/src/test/java/onlineTraining/RelativeLocatorMakeMyTrip.java
@@ -3,13 +3,29 @@ package onlineTraining;
 import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class RelativeLocatorMakeMyTrip {
+	@FindBy(xpath=("")) private WebElement mmtPopUp;
+	public WebElement getMmtPopUp()
+	{
+		return mmtPopUp;
+	}
+	public RelativeLocatorMakeMyTrip(WebDriver driver) {
+		PageFactory.initElements(driver,this);
+	}
+	public void removePopup()
+	{
+		
+	}
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		driver.get("");
+		driver.get("https://www.makemytrip.com/");
+		
 	}
 }
