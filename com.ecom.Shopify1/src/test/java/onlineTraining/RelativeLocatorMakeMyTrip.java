956fff7232f71e07a1caacf84d4717d05d225eef
Sumit021995 <kaku.saurav@gmail.com>
2025-02-12 08:59:20 +0530

    new commit

---
+++ com.ecom.Shopify1/src/test/java/onlineTraining/RelativeLocatorMakeMyTrip.java
@@ -0,0 +1,15 @@
+package onlineTraining;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class RelativeLocatorMakeMyTrip {
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("");
+	}
+}
