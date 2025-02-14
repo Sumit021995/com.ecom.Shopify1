4d51e25099ea06355dc08ddfad035ae18fd11704
Sumit021995 <kaku.saurav@gmail.com>
2025-02-14 21:15:59 +0530

    new commit

--- com.ecom.Shopify1/src/test/java/trainingUdemy/ArrayWithScanner.java
+++ com.ecom.Shopify1/src/test/java/trainingUdemy/ArrayWithScanner.java
@@ -1,20 +1,9 @@
 package trainingUdemy;
 
 import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
-/**
- * Algorithm 1: Add  numbers entered by the user in an array
 
-Step 1: Start
-Step 2: Declare integer array and sum.
-Step 3: Read values num1 and num2.
-Step 4: Add num1 and num2 and assign the result to sum.
-        sum←num1+num2
-Step 5: Display sum
-Step 6: Stop
- */
 public class ArrayWithScanner {
 	public static void main(String[] args) {
 		System.out.println("Program Starts");
