37fc6fb3aa1d7a47a3a8de46daea5cb9e6620aed
Sumit021995 <kaku.saurav@gmail.com>
2025-02-14 21:22:54 +0530

    new commit

--- com.ecom.Shopify1/src/test/java/onlineTraining/NestedFrameCode.java
+++ com.ecom.Shopify1/src/test/java/onlineTraining/NestedFrameCode.java
@@ -16,8 +16,7 @@ public class NestedFrameCode {
 		driver.get("file:///C:/Users/sumit/OneDrive/Desktop/mainPage.html");
 		driver.switchTo().frame(0);
 		WebElement textFieldUserName =driver.findElement(By.xpath("//input"));	
-		textFieldUserName.sendKeys("Sumit021995",Keys.TAB,"Sumit@123",Keys.TAB,Keys.TAB,"Sumit Saurav");
-		
+		textFieldUserName.sendKeys("Sumit021995",Keys.TAB,"Sumit@123",Keys.TAB,Keys.TAB,"Sumit Saurav");	
 		Thread.sleep(3000);
 		driver.quit();
 		
