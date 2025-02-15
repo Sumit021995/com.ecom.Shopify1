329c9abfbea70dc47936ad6e7b7630afd6a14e05
Sumit021995 <kaku.saurav@gmail.com>
2025-02-15 20:37:49 +0530

    new commit

--- com.ecom.Shopify1/src/test/java/onlineTraining/NestedFrame4.java
+++ com.ecom.Shopify1/src/test/java/onlineTraining/NestedFrame4.java
@@ -3,8 +3,10 @@ package onlineTraining;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class NestedFrame4 {
 	public static void main(String[] args) throws Exception {
@@ -14,14 +16,9 @@ public class NestedFrame4 {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("file:///C:/Users/sumit/OneDrive/Desktop/mainPage.html");
 		driver.switchTo().frame(0);
-		driver.findElement(By.xpath("//input[1]")).sendKeys("Sumit021995");
-		driver.switchTo().frame(0);
-		driver.findElement(By.xpath("//input")).sendKeys("Sumit Saurav");
-		driver.switchTo().parentFrame();
-		driver.findElement(By.xpath("//input[2]")).sendKeys("Sumit@123");
-		driver.switchTo().defaultContent();
-		driver.findElement(By.partialLinkText("Google")).click();
-		
+		Actions act = new Actions(driver);
+		act.moveToLocation(485, 155).click().sendKeys("Sumit021995",Keys.TAB,"Sumit@123",Keys.TAB,Keys.TAB,"Sumit Saurav").perform();
+
 		Thread.sleep(2000);
 		driver.quit();
 		
