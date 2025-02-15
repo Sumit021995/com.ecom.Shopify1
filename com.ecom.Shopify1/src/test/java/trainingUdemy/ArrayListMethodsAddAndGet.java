a081b5c43b0b174df4f84f2ea1693cf64e9c5e58
Sumit021995 <kaku.saurav@gmail.com>
2025-02-16 01:22:53 +0530

    new commit

---
+++ com.ecom.Shopify1/src/test/java/trainingUdemy/ArrayListMethodsAddAndGet.java
@@ -0,0 +1,20 @@
+package trainingUdemy;
+
+import java.util.ArrayList;
+
+public class ArrayListMethodsAddAndGet {
+	public static void main(String[] args) {
+		ArrayList al = new ArrayList();
+		al.add("Sumit Saurav");
+		al.add(29);
+		al.add("Test Engineer");
+		for(Object o:al)
+		{
+			System.out.println(o);
+		}
+		for(int)
+		{
+			System.out.println(o);
+		}
+	}
+}
