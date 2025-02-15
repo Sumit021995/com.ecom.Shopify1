3e5a79767bd01c1b6a42ea64b77e949543878b00
Sumit021995 <kaku.saurav@gmail.com>
2025-02-15 20:22:50 +0530

    new commit

--- com.ecom.Shopify1/src/test/java/onlineTraining/NestedFrame2.java
+++ com.ecom.Shopify1/src/test/java/onlineTraining/NestedFrame2.java
@@ -14,7 +14,6 @@ public class NestedFrame2 {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("file:///C:/Users/sumit/OneDrive/Desktop/mainPage.html");
 		driver.switchTo().frame(0);
-//		Thread.sleep(2000);
 		driver.findElement(By.xpath("//body[contains(text(),'username')]/input[1]")).sendKeys("Sumit021995");
 		driver.switchTo().frame(0);
 		driver.findElement(By.xpath("//input")).sendKeys("Sumit Saurav");
