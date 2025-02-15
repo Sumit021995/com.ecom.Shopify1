5eaa882d77abd61afc0cf723935abe169c1f7fd2
Sumit021995 <kaku.saurav@gmail.com>
2025-02-15 20:47:32 +0530

    new commit

--- com.ecom.Shopify1/src/test/java/onlineTraining/NestedFrame4.java
+++ com.ecom.Shopify1/src/test/java/onlineTraining/NestedFrame4.java
@@ -8,23 +8,19 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
-public class NestedFrame4 {
-	public static void main(String[] args) throws Exception {
-		
+public class NestedFrame4 
+{
+	public static void main(String[] args) throws Exception 
+	{
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("file:///C:/Users/sumit/OneDrive/Desktop/mainPage.html");
-//		driver.switchTo().frame(0);
 		Actions act = new Actions(driver);
 		act.moveToLocation(485, 155).click().sendKeys("Sumit021995",Keys.TAB,"Sumit@123",Keys.TAB,Keys.TAB,"Sumit Saurav").perform();
-//		driver.switchTo().defaultContent();
 		driver.findElement(By.partialLinkText("Google")).click();
-
 		Thread.sleep(2000);
 		driver.quit();
-		
-		
 	}
 }
 
