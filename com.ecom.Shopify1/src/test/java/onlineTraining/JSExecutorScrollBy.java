eedd4890b64d1164c815a0db2738df41225fcdad
Sumit021995 <kaku.saurav@gmail.com>
2025-02-17 07:57:50 +0530

    new commit

--- com.ecom.Shopify1/src/test/java/onlineTraining/JSExecutorScrollBy.java
+++ com.ecom.Shopify1/src/test/java/onlineTraining/JSExecutorScrollBy.java
@@ -1,7 +1,10 @@
 package onlineTraining;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class JSExecutorScrollBy {
 	public static void main(String[] args) {
-		
+		WebDriver driver = new ChromeDriver();
 	}
 }
