fb0c751326bba5d4dcf0afa8d7d3913795665839
Sumit021995 <kaku.saurav@gmail.com>
2025-02-17 08:02:27 +0530

    new commit

--- com.ecom.Shopify1/src/test/java/onlineTraining/JSExecutorScrollBy.java
+++ com.ecom.Shopify1/src/test/java/onlineTraining/JSExecutorScrollBy.java
@@ -1,5 +1,8 @@
 package onlineTraining;
 
+import java.time.Duration;
+
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -7,5 +10,8 @@ public class JSExecutorScrollBy {
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://demowebshop.tricentis.com/");
+		JavascriptExecutor jse = (JavascriptExecutor)driver;
 	}
 }
