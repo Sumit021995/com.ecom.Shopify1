526437e8f727402ea52b8958066cf46f651f2f0d
Sumit021995 <kaku.saurav@gmail.com>
2025-02-17 08:24:50 +0530

    new commit

---
+++ com.ecom.Shopify1/src/test/java/onlineTraining/ScrollIntoMethodJSExecutor.java
@@ -0,0 +1,30 @@
+package onlineTraining;
+
+import java.time.Duration;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ScrollIntoMethodJSExecutor {
+	public static void main(String[] args) throws InterruptedException {
+		System.out.println("Program Starts");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://demowebshop.tricentis.com/");
+		JavascriptExecutor jse = (JavascriptExecutor)driver;
+		Thread.sleep(2000);
+		jse.executeScript("window.scrollTo(0,500)");
+		Thread.sleep(2000);
+		jse.executeScript("window.scrollBy(0,500)");
+		Thread.sleep(2000);
+		jse.executeScript("window.scrollTo(0,200)");
+		Thread.sleep(2000);
+		driver.quit();
+		System.out.println("Program Ends");
+		
+	}
+}
+
+
