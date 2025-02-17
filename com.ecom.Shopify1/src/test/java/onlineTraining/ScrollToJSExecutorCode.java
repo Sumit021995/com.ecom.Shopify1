ce13477c61467722d5f9520dd253da95798d989e
Sumit021995 <kaku.saurav@gmail.com>
2025-02-17 08:15:13 +0530

    new commit

--- com.ecom.Shopify1/src/test/java/onlineTraining/ScrollToJSExecutorCode.java
+++ com.ecom.Shopify1/src/test/java/onlineTraining/ScrollToJSExecutorCode.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ScrollToJSExecutorCode {
 	public static void main(String[] args) throws InterruptedException {
+		System.out.println("Program Starts");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
