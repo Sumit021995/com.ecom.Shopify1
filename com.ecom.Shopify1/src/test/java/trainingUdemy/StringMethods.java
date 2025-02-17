2b3140a24530fec464dd0c00e4fa1b7f357b8e94
Sumit021995 <kaku.saurav@gmail.com>
2025-02-17 22:41:26 +0530

    new commit

--- com.ecom.Shopify1/src/test/java/trainingUdemy/StringMethods.java
+++ com.ecom.Shopify1/src/test/java/trainingUdemy/StringMethods.java
@@ -1,13 +1,19 @@
 package trainingUdemy;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class StringMethods {
 	public static void main(String[] args) {
 		String sample = "Sumit Saurav is my name";
-		String newSample = new String("This String is created by New Key word");
-		String[] sampleArray = sample.split(" ");
-		for(String s:sampleArray)
+		String newSample1 = new String("This String is created by New Key word");
+		String[] newSample = newSample1.split(" ");
+		for(String s:newSample)
 		{
 			System.out.println(s);
 		}
+		List<String> listOfSampleArray = Arrays.asList(newSample);
+		System.out.println(listOfSampleArray.contains("Key"));
+		
 	}
 }
