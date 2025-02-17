60f004028f0f838b654c24cfe2cdb85c1c057663
Sumit021995 <kaku.saurav@gmail.com>
2025-02-17 08:04:29 +0530

    new commit

--- com.ecom.Shopify1/src/test/java/onlineTraining/JSExecutorScrollBy.java
+++ com.ecom.Shopify1/src/test/java/onlineTraining/JSExecutorScrollBy.java
@@ -7,12 +7,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class JSExecutorScrollBy {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("https://demowebshop.tricentis.com/");
 		JavascriptExecutor jse = (JavascriptExecutor)driver;
+		Thread.sleep(2000);
 		jse.executeScript("window.scrollBy(0,500)");
+		jse.executeScript("window.scrollBy(0,500)");
+		jse.executeScript("window.scrollBy(0,-500)");
+		
 	}
 }
