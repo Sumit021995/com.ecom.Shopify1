fd571ab949c533d88fa4135a1297d0b2b40ebbac
Sumit021995 <kaku.saurav@gmail.com>
2025-02-19 07:50:40 +0530

    new commit

---
+++ com.ecom.Shopify1/src/test/java/onlineTraining/ScrollIntoViewWebElementJSExecutor.java
@@ -0,0 +1,31 @@
+package onlineTraining;
+
+import java.time.Duration;
+
+import org.checkerframework.checker.units.qual.t;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ScrollIntoViewWebElementJSExecutor {
+	public static void main(String[] args) throws InterruptedException {
+		System.out.println("Program Starts");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://demowebshop.tricentis.com/");
+		WebElement textEle = driver.findElement(By.xpath("//h2[contains(text(),'Welcome to')]"));
+		JavascriptExecutor jse = (JavascriptExecutor)driver;
+		Thread.sleep(2000);
+		jse.executeScript("arguments[0].scrollIntoView(false)",textEle);
+		Thread.sleep(3000);
+		driver.quit();
+		System.out.println("Program Ends");
+		
+	}
+}
+
+
+
