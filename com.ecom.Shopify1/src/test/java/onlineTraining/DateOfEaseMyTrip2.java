4ee16110af4865850eddf9eff66cf18375f194bf
Sumit021995 <kaku.saurav@gmail.com>
2025-02-20 09:41:02 +0530

    new commit

---
+++ com.ecom.Shopify1/src/test/java/onlineTraining/DateOfEaseMyTrip2.java
@@ -0,0 +1,35 @@
+package onlineTraining;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class DateOfEaseMyTrip2 {
+	public static void main(String[] args) throws InterruptedException {
+		ChromeOptions option = new ChromeOptions();
+		option.addArguments("--disable-notifications");
+		WebDriver driver = new ChromeDriver(option);
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://www.easemytrip.com/");
+		driver.findElement(By.id("ddate")).click();
+		while(true)
+		{
+			try
+			{
+				driver.findElement(By.id("frth_6_24/05/2025")).click();
+				break;
+			}
+			catch(Exception e)
+			{
+				driver.findElement(By.id("img2Nex")).click();
+			}
+			
+		}
+		Thread.sleep(2000);
+//		driver.quit();
+	}
+}
