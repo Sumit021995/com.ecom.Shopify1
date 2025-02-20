baddd87d320dbecff5eac38213f03b0a71a49c7d
Sumit021995 <kaku.saurav@gmail.com>
2025-02-20 09:41:27 +0530

    new commit

--- com.ecom.Shopify1/src/test/java/onlineTraining/ChromeOptionsEasyMyTrip.java
+++ com.ecom.Shopify1/src/test/java/onlineTraining/ChromeOptionsEasyMyTrip.java
@@ -2,18 +2,23 @@ package onlineTraining;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class ChromeOptionsEasyMyTrip {
 	public static void main(String[] args) {
 		ChromeOptions option = new ChromeOptions();
-		option.addArguments("--disable-notifications");
+//		option.addArguments("--disable-notifications");
 //		option.addArguments("--incognito");
-		WebDriver driver = new ChromeDriver();
+		option.addArguments("--headless");
+		WebDriver driver = new ChromeDriver(option);
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 		driver.get("https://www.easemytrip.com/");
+		WebElement ele = driver.findElement(By.id("dday"));
+		System.out.println(ele.getText());
 	}
 }
