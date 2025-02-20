b62d4d842b756e502934cb3faf598cd0d33db738
Sumit021995 <kaku.saurav@gmail.com>
2025-02-20 21:50:23 +0530

    new commit

--- com.ecom.Shopify1/src/test/java/onlineTraining/DateOfEaseMyTrip2.java
+++ com.ecom.Shopify1/src/test/java/onlineTraining/DateOfEaseMyTrip2.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class DateOfEaseMyTrip2 {
 	public static void main(String[] args) throws Exception {
 		String expectedMonth ="April";
-		String year ="2025";
+		String expectedYear ="2025";
 		String date ="24";
 		
 		ChromeOptions option = new ChromeOptions();
