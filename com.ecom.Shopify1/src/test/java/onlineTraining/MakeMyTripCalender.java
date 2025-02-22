9fe00d439d596881f68f66a26acd8c6a1f893260
Sumit021995 <kaku.saurav@gmail.com>
2025-02-22 10:24:37 +0530

    new commit

--- com.ecom.Shopify1/src/test/java/onlineTraining/MakeMyTripCalender.java
+++ com.ecom.Shopify1/src/test/java/onlineTraining/MakeMyTripCalender.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class MakeMyTripCalender {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 		System.out.println("Program Starts");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
@@ -15,8 +15,15 @@ public class MakeMyTripCalender {
 		driver.get("https://www.makemytrip.com/");
 		driver.findElement(By.className("commonModal__close")).click();
 		driver.findElement(By.id("fromCity")).click();
-		driver.findElement(By.id("react-autowhatever-1-section-0-item-0")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@placeholder='From']")).sendKeys("Kolkata");
 		
+		Thread.sleep(2000);
+		driver.findElement(By.id("react-autowhatever-1-section-0-item-0")).click();
+		driver.findElement(By.id("fromCity")).click();
+		
+		Thread.sleep(2000);
+		driver.quit();
 		System.out.println("Program Ends");
 	}
 }
