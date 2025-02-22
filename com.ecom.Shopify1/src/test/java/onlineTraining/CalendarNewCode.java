a40bda94e8a425c41eb7c6c2a69a7dd77ac936eb
Sumit021995 <kaku.saurav@gmail.com>
2025-02-22 08:22:51 +0530

    new commit

--- com.ecom.Shopify1/src/test/java/onlineTraining/CalendarNewCode.java
+++ com.ecom.Shopify1/src/test/java/onlineTraining/CalendarNewCode.java
@@ -2,6 +2,7 @@ package onlineTraining;
 
 import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.Calendar;
 import java.util.Date;
 
 import org.openqa.selenium.WebDriver;
@@ -11,16 +12,17 @@ public class CalendarNewCode {
 	public static void main(String[] args) {
 		System.out.println("Program Starts");
 		WebDriver driver = new ChromeDriver();
-//		Options opt = driver.manage();
-//		Window win = opt.window();
-//		win.maximize();
-////		Point p = new Point(0, 0);
-////		Dimension d = new Dimension(0, 0);
+		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		Date d= new Date();
-		SimpleDateFormat sdf = new SimpleDateFormat("YYYYMMdd_hhmmss");
-		String value = sdf.format(d);
+		driver.get("https://demoapps.qspiders.com/ui/datePick?sublist=0");
+		Date dateObj= new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-YY");
+		String todayDate = sdf.format(dateObj);
+		System.out.println(todayDate);
+		Calendar cal = sdf.getCalendar();
+		cal.add(Calendar.DAY_OF_MONTH, 7);
+		String requestDate = sdf.format(cal.getTime());
 		//ScreenShot name Screenshot_20250219_165653
-		System.out.println(value);
+		System.out.println(requestDate);
 	}
 }
