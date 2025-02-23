22e2f914beb68001f2ebf21cee5eedf7a39e189d
Sumit021995 <kaku.saurav@gmail.com>
2025-02-24 00:38:17 +0530

    new commit

--- com.ecom.Shopify1/src/test/java/onlineTraining/MakeMyTripCalender.java
+++ com.ecom.Shopify1/src/test/java/onlineTraining/MakeMyTripCalender.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class MakeMyTripCalender {
 	public static void main(String[] args) throws Exception {
 		Date d = new Date();
-		SimpleDateFormat sdf =
+		SimpleDateFormat sdf = new SimpleDateFormat("");
 		String expectedMonthAndYear="April 2025";
 		System.out.println("Program Starts");
 		WebDriver driver = new ChromeDriver();
