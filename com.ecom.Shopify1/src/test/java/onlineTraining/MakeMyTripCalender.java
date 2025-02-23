5f50e1dbb234f3092745f3eecd933df51569e77a
Sumit021995 <kaku.saurav@gmail.com>
2025-02-23 23:12:33 +0530

    new commit

--- com.ecom.Shopify1/src/test/java/onlineTraining/MakeMyTripCalender.java
+++ com.ecom.Shopify1/src/test/java/onlineTraining/MakeMyTripCalender.java
@@ -25,7 +25,8 @@ public class MakeMyTripCalender {
 		driver.findElement(By.xpath("//input[@placeholder='To']")).sendKeys("Bengaluru");
 		Thread.sleep(2000);
 		driver.findElement(By.id("react-autowhatever-1-section-0-item-0")).click();
-		driver.findElement(By.xpath("//div[@class='DayPicker-Month']/div/div[text()='February 2025']")).	
+		driver.findElement(By.xpath("//div[@class='DayPicker-Month']/div/div[text()='February 2025']"));	
+		driver.findElement(By.xpath("//span[@aria-label='Next Month']"));	
 		Thread.sleep(2000);
 //		driver.quit();
 		System.out.println("Program Ends");
