a05586738b56e5e0dfa82893d3c8a114b2a25e67
Sumit021995 <kaku.saurav@gmail.com>
2025-02-25 00:05:25 +0530

    new commit

--- com.ecom.Shopify1/src/test/java/ddtStarted/PropertiesUtility.java
+++ com.ecom.Shopify1/src/test/java/ddtStarted/PropertiesUtility.java
@@ -5,6 +5,7 @@ import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class PropertiesUtility {
@@ -26,7 +27,8 @@ public class PropertiesUtility {
 			driver=new ChromeDriver();
 		else if(browser.equalsIgnoreCase("firefox"))
 			driver=new FirefoxDriver();
-		else if
-			
+		else if (browser.equalsIgnoreCase("Edge"))
+			driver = new EdgeDriver();
+		else driver = new ChromeDriver();
 	}
 }
