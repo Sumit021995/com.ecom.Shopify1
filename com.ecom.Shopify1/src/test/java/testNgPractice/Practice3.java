4bf079a533133f94f4ed6e00440d6f705ee2ae96
Sumit021995 <kaku.saurav@gmail.com>
2025-03-01 07:41:55 +0530

    new commit

---
+++ com.ecom.Shopify1/src/test/java/testNgPractice/Practice3.java
@@ -0,0 +1,30 @@
+package testNgPractice;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+
+public class Practice3 {
+	
+	@Test
+	public void navigateToGithub() throws InterruptedException
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://github.com/Sumit021995/");
+		Reporter.log("Navigated to Github",true);
+		Thread.sleep(2000);
+		driver.quit();
+	}
+	@Test
+	public void navigateToFacebook() throws InterruptedException
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		Reporter.log("Navigated to Facebook",true);
+		Thread.sleep(2000);
+		driver.quit();
+		
+	}
+}
