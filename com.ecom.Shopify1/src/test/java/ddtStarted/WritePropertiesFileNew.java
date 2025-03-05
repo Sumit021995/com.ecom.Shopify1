47e4fb152f2d3e06c732b795a49051183b62f947
Sumit021995 <kaku.saurav@gmail.com>
2025-03-05 16:40:05 +0530

    new commit

--- com.ecom.Shopify1/src/test/java/ddtStarted/WritePropertiesFileNew.java
+++ com.ecom.Shopify1/src/test/java/ddtStarted/WritePropertiesFileNew.java
@@ -5,8 +5,11 @@ import java.util.Properties;
 
 public class WritePropertiesFileNew {
 	public static void main(String[] args) throws Exception {
+		
 		FileOutputStream fos = new FileOutputStream("C:\\Users\\sumit\\OneDrive\\Desktop\\DDT\\TestData1.properties");
 		Properties prop = new Properties();
+	
+		
 		prop.setProperty("browser", "edge");
 		prop.setProperty("url", "https://github.com/login");
 		prop.store(fos, " Configuration Data");
